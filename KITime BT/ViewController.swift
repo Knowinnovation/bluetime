594dbb598cf4a095d0a2704ba16e750824c69ed2
Drew Dunne <drew@drewsdunne.com>
2016-05-30 15:26:32 -0400

    Initial Commit

---
+++ KITime BT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KITime BT
+//
+//  Created by Drew Dunne on 5/30/16.
+//  Copyright © 2016 Know Innovation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
